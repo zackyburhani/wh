7f9fa33b6b9b75e04ba4d22ba0ebdb3a3c227315
faizalvian <faizalvian97@gmail.com>
2018-05-17 12:45:18 +0700

    tambah user dan user group

--- add_warehouse.php
+++ add_warehouse.php
@@ -44,7 +44,7 @@
      </div>
   </div>
    <div class="row">
-    <div class="col-md-5">
+    <div class="col-md-12">
       <div class="panel panel-default">
         <div class="panel-heading">
           <strong>
